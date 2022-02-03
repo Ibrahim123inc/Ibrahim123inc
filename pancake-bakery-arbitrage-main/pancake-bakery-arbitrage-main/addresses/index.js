935cf1c132fa8dad6bcc22d0565003df7e48383d
Ibrahim123inc <74814709+Ibrahim123inc@users.noreply.github.com>
2022-02-03 04:53:41 -0800

    flash swap

---
+++ pancake-bakery-arbitrage-main/pancake-bakery-arbitrage-main/addresses/index.js
@@ -0,0 +1,12 @@
+const pancakeMainnet = require('./pancake-mainnet.json');
+const bakeryMainnet = require('./bakery-mainnet.json');
+const apeMainnet = require('./ape-mainnet.json');
+
+module.exports = {
+    mainnet: {
+        pancake: pancakeMainnet,
+        bakery: bakeryMainnet,
+        ape: apeMainnet,
+    }
+};
+
